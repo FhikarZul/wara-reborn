dba3f9bdbedfbad8c4f5c500cd3fdfdc7022acd7
LackFos <LackFosisOnline@gmail.com>
2025-06-25 23:49:52 +0700

    Service/DetectionService: - Add logic to remove unnecessary part from ingredients text

--- Wara/Service/DetectionService.swift
+++ Wara/Service/DetectionService.swift
@@ -17,14 +17,6 @@ class DetectionService {
     }
     
     // MARK: - Methods
-    func hasIngredientsLabel(in text: String) -> Bool {
-        // Kita bisa mencari kedua variasi untuk hasil yang lebih baik
-        let keyword1 = "원재료" // Raw Materials
-        let keyword2 = "원재료명" // Raw Material Name, sering digunakan juga
-        
-        return text.contains(keyword1) || text.contains(keyword2)
-    }
-    
     func analyzeIngredients(text: String) async -> DetectionResult {
         // Check are the text contain ingredient keyword
         let containsIngredientsLabel = hasIngredientsLabel(in: text)
@@ -33,24 +25,16 @@ class DetectionService {
             return DetectionResult(status: .ingredientsNotFound, foundIngredients: [], originalText: text)
         }
         
-        // Jika kata kunci ditemukan, analisis seluruh teks
-        var cleanedText = text.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "\n", with: "")
+        // Remove line break of OCR result text
+        let cleanedText = text.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "\n", with: "")
         
-        let pattern = "(원재료|원재료명)(.*?)(?=제품명|식품유형|제조원|유통전문|소비기한|원재료명|포장재질|품목보고번호|$)"
+        // Extract ingredients part only
+        guard let ingredientFullText = self.extractIngredients(from: cleanedText) else {
+            return DetectionResult(status: .ingredientsNotFound, foundIngredients: [], originalText: text)
+        }
         
-        do {
-            let regex = try NSRegularExpression(pattern: pattern, options: [.dotMatchesLineSeparators])
-            let nsrange = NSRange(text.startIndex..<text.endIndex, in: text)
-            
-            if let match = regex.firstMatch(in: text, options: [], range: nsrange) {
-                if let range = Range(match.range(at: 2), in: text) {
-                    cleanedText = String(text[range])
-                }
-            } else {
-                print("No match found.")
-            }
-        } catch {
-            print("Invalid regex: \(error)")
+        guard let cleanedIngredientFullText = self.removeIngredientExtraInformation(from: ingredientFullText) else {
+            return DetectionResult(status: .ingredientsNotFound, foundIngredients: [], originalText: text)
         }
         
         let descriptor = FetchDescriptor<Ingredient>()
@@ -60,7 +44,7 @@ class DetectionService {
         
         var foundItems: [Ingredient] = []
         for ingredient in allIngredients {
-            if cleanedText.contains(ingredient.koreanName) {
+            if cleanedIngredientFullText.contains(ingredient.koreanName) {
                 foundItems.append(ingredient)
             }
         }
@@ -75,6 +59,39 @@ class DetectionService {
             return DetectionResult(status: .raguRagu, foundIngredients: foundItems, originalText: text)
         } else {
             return DetectionResult(status: .aman, foundIngredients: foundItems, originalText: text)
+        }
+    }
+    
+    func hasIngredientsLabel(in text: String) -> Bool {
+        let keyword1 = "원재료" // Raw Materials
+        let keyword2 = "원재료명" // Raw Material Name, sering digunakan juga
+        return text.contains(keyword1) || text.contains(keyword2)
+    }
+    
+    func extractIngredients(from text: String) -> String? {
+        let pattern = "(원재료|원재료명)(.*?)(?=제품명|식품유형|제조원|유통전문|소비기한|원재료명|포장재질|품목보고번호|$)"
+        
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [.dotMatchesLineSeparators])
+            let nsrange = NSRange(text.startIndex..<text.endIndex, in: text)
+            
+            if let match = regex.firstMatch(in: text, options: [], range: nsrange) {
+                if let range = Range(match.range(at: 2), in: text) {
+                    return String(text[range])
+                }
+            }
+        } catch {
+            print("Invalid regex: \(error)")
+        }
+        
+        return nil
+    }
+    
+    func removeIngredientExtraInformation(from text: String) -> String? {
+        let pattern = #/\((.*?)\)|(\s*\d*\s*%)|\[|\]?/#
+        
+        return text.replacing(pattern) { match in
+            return ""
         }
     }
 }
